889d45d09f7747866f940eb664d34f52a0534007
Alex Nguyen <alexngn2@gmail.com>
2020-01-30 21:12:39 -0700

    thing

---
+++ playerimages.js
@@ -0,0 +1,23 @@
+
+
+function displayTracks(tracks) {
+
+    for (var i = 0; i < tracks.length; i++) {
+        var trackName = tracks[i].name;
+        var artistName = tracks[i].artists[0].name;
+        var image = tracks[i].album.images[0].url;
+        console.log(trackName);
+
+        $("#song-list").append(`<li class="list_item choice" id="0" data-title="${trackName}" data-artist="">
+        <div class="thumb"><img src="${image}"> </div>
+        <div class="info"> 
+          <div class="title">${trackName}</div>
+          <div class="artist">${artistName}</div>
+        </div>
+      </li>`);
+    
+    }
+
+}
+
+
--- script-4.js
+++ script-4.js
@@ -6,50 +6,67 @@ var musixMatchQueryArray = [];
 
 $(document).ready(function () {
 
-    requestToken();
-    getToken();
+  requestToken();
+  getToken();
 
 });
 
 function requestToken() {
 
-    var redirectUri = window.location.href;
-    if (redirectUri.includes("#access_token=")) return;
+  var redirectUri = window.location.href;
+  if (redirectUri.includes("#access_token=")) return;
 
-    if (redirectUri !== "https://sk2060sk2060.github.io/group-project-1" || redirectUri !== "http://127.0.0.1:8080/") {
-        redirectUri = "http://127.0.0.1:8080/"
+  if (redirectUri !== "https://sk2060sk2060.github.io/group-project-1" || redirectUri !== "http://127.0.0.1:8080/") {
+    redirectUri = "http://127.0.0.1:8080/"
 
-        var scope = "user-library-modify";
-        window.location.href = "https://accounts.spotify.com/authorize?client_id=21b6e99dfa9948818d67377855f4d685&response_type=token&scope=" + encodeURIComponent(scope) + "&redirect_uri=" + redirectUri;
-    }
+    var scope = "user-library-modify";
+    window.location.href = "https://accounts.spotify.com/authorize?client_id=21b6e99dfa9948818d67377855f4d685&response_type=token&scope=" + encodeURIComponent(scope) + "&redirect_uri=" + redirectUri;
+  }
 }
 function getToken() {
-    var returnAuthorizationToken = location.hash.substr(1);
-    authorizationToken = "Bearer " + returnAuthorizationToken.substring(returnAuthorizationToken.indexOf("=") + 1, returnAuthorizationToken.indexOf("&"));
-    console.log(authorizationToken);
+  var returnAuthorizationToken = location.hash.substr(1);
+  authorizationToken = "Bearer " + returnAuthorizationToken.substring(returnAuthorizationToken.indexOf("=") + 1, returnAuthorizationToken.indexOf("&"));
+  console.log(authorizationToken);
 
 }
 
 function buildQueryURL() {
-    searchTerm = $("#search-term").val();
-    console.log(searchTerm);
-    queryURL = "https://api.spotify.com/v1/search?q=" + searchTerm + "&type=artist";
+  searchTerm = $("#search-term").val();
+  console.log(searchTerm);
+  queryURL = "https://api.spotify.com/v1/search?q=" + searchTerm + "&type=artist";
 }
 
 function searchForArtist() {
-    buildQueryURL();
+  buildQueryURL();
+  $.ajax({
+    method: "GET",
+    // headers: {
+    //     "Authorization": authorizationToken
+    // },
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", authorizationToken);
+      request.setRequestHeader("Accept", "application/json");
+    },
+    url: queryURL,
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+  }).done(function (response) {
+    console.log(response);
+    var firstSongID = response.artists.items[0].id;
+    queryURL = `https://api.spotify.com/v1/artists/${firstSongID}/top-tracks?country=US`;
+
     $.ajax({
-        method: "GET",
-        // headers: {
-        //     "Authorization": authorizationToken
-        // },
-        beforeSend: function (request) {
-            request.setRequestHeader("Authorization", authorizationToken);
-            request.setRequestHeader("Accept", "application/json");
-        },
-        url: queryURL,
+      method: "GET",
+      // headers: {
+      //     "Authorization": authorizationToken
+      // },
+      beforeSend: function (request) {
+        request.setRequestHeader("Authorization", authorizationToken);
+        request.setRequestHeader("Accept", "application/json");
+      },
+      url: queryURL,
     }).fail(function (jqXHR, textStatus, errorThrown) {
     }).done(function (response) {
+<<<<<<< HEAD
         console.log(response);
         var firstSongID = response.artists.items[0].id;
         queryURL = `https://api.spotify.com/v1/artists/${firstSongID}/top-tracks?country=US`;
@@ -81,17 +98,38 @@ function searchForArtist() {
           }
         });
 
+=======
+      var openPlayer = document.querySelector(".btn-open-player");
+      openPlayer.click();
+      console.log({ response }, 'Clikc Response')
+      var tracks = response.tracks;
+
+      // var trackItems = document.getElementsByClassName("list_item");
+      // for (var i = 0; i < trackItems.length; i++) {
+      //   trackItems[i].id = i;
+      //   var artist = response.tracks[i].artists[0].name;
+      //   var title = response.tracks[i].name;
+      //   var musixMatchQuery = `${artist} - ${title}`;
+      //   musixMatchQueryArray.push(musixMatchQuery);
+      //   trackItems[i].querySelector(".title").textContent = title;
+      //   trackItems[i].querySelector(".artist").textContent = artist;
+      // }
+      displayTracks(tracks);
+>>>>>>> 42d4a2bb1401e6e729555ac51c86f989eb2b4cee
     });
+
+  });
 }
 
 
 document.getElementById("search").addEventListener("click", function () {
-    searchForArtist();
+  searchForArtist();
 });
 
 
 
 // First way 
+<<<<<<< HEAD
 const listItem = $(".list")
 $(document).on("click", ".list_item",function(){
  var songUri = this.getAttribute("trackuri");
@@ -101,13 +139,27 @@ $(document).on("click", ".list_item",function(){
 
 const listEl = document.querySelector("#song-list");
 listEl.addEventListener("click", function(event) {
+=======
+listEl.addEventListener("click", function (event) {
+>>>>>>> 42d4a2bb1401e6e729555ac51c86f989eb2b4cee
   if (event.target.matches("li")) {
     console.log(event.target.id);
     var listItem = event.target;
     var artist = listItem.querySelector(".artist").textContent;
     var title = listItem.querySelector(".title").textContent;
     var musixMatchQuery = `${artist} - ${title}`;
+    $("#curator").append(`<div class="curator_title_wrapper"><span>LP</span>
+<div class="curator_line"></div>
+<div class="info"></div>
+<div class="playback_info"></div>
+<div class="title">${title}</div>
+<div class="artist">${artist}</div>
+<div class="curator_line"></div><span>20</span>
+</div>`);
     getLyricsAndDisplay(musixMatchQuery);
+
+
+
   }
 });
 function addTrackToPlayer(songUri) {
@@ -134,67 +186,66 @@ $(".playback_wrapper").html(player);
 // });
 
 
-function getLyricsAndDisplay(query){
+function getLyricsAndDisplay(query) {
   var trackSearch = query;
   document.getElementById("lyrics").innerHTML = "";
-    $.ajax({
-      type: "GET",
-      data: {
-        apikey:"da9deef32d4c04ca1b56d484548bdf76",
-        q_track_artist: trackSearch,
-        format:"jsonp",
-        callback:"jsonp_callback"
-      },
-      url: "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1&apikey=https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=0&apikey=da9deef32d4c04ca1b56d484548bdf76",
-      dataType: "jsonp",
-      jsonpCallback: 'jsonp_callback',
-      contentType: 'application/json',
-      success: function(data) {
-        console.log(data); 
-        var rand = data.message.body.track_list[0];
-        var thisTrack = (rand.track.track_id)
-        getLyricsNoww(thisTrack);    
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-      }    
+  $.ajax({
+    type: "GET",
+    data: {
+      apikey: "da9deef32d4c04ca1b56d484548bdf76",
+      q_track_artist: trackSearch,
+      format: "jsonp",
+      callback: "jsonp_callback"
+    },
+    url: "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1&apikey=https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=0&apikey=da9deef32d4c04ca1b56d484548bdf76",
+    dataType: "jsonp",
+    jsonpCallback: 'jsonp_callback',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+      var rand = data.message.body.track_list[0];
+      var thisTrack = (rand.track.track_id)
+      getLyricsNoww(thisTrack);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
   });
 };
 
-function getLyricsNoww(track){
+function getLyricsNoww(track) {
   var trackId = track;
   console.log(trackId)
   $.ajax({
     type: "GET",
     data: {
-      apikey:"da9deef32d4c04ca1b56d484548bdf76",
+      apikey: "da9deef32d4c04ca1b56d484548bdf76",
       track_id: trackId,
-      format:"jsonp",
-      callback:"jsonp_callback"
+      format: "jsonp",
+      callback: "jsonp_callback"
     },
     url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=0&apikey=https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=0&apikey=da9deef32d4c04ca1b56d484548bdf76",
     dataType: "jsonp",
     jsonpCallback: 'jsonp_callback',
     contentType: 'application/json',
-    success: function(data) {
-      console.log(data); 
-      console.log(data.message.body.lyrics.lyrics_body); 
-      var lyricsBody = data.message.body.lyrics.lyrics_body.split(/\s+/).slice(0,100).join(" ")+ "...";
+    success: function (data) {
+      console.log(data);
+      console.log(data.message.body.lyrics.lyrics_body);
+      var lyricsBody = data.message.body.lyrics.lyrics_body.split(/\s+/).slice(0, 100).join(" ") + "...";
       var j = document.createElement("div")
       j.setAttribute("style", "white-space: pre-wrap; font-size: 20px; color: brown;");
       j.textContent = lyricsBody
       document.getElementById("lyrics").appendChild(j)
     },
-    error: function(jqXHR, textStatus, errorThrown) {
+    error: function (jqXHR, textStatus, errorThrown) {
       console.log(jqXHR);
       console.log(textStatus);
       console.log(errorThrown);
-    }    
+    }
   });
- };
+};
 
 
-        
-   
+
