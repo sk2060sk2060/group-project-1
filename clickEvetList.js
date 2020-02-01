7101047d5cc8cf6a6f0f98d7e4d3b904c67fc836
sk <sk2060sk2060@gmail.com>
2020-02-01 10:47:23 -0700

    display_lyrics

--- clickEvetList.js
+++ clickEvetList.js
@@ -16,5 +16,26 @@ $(document).on("click", ".choice" , function() {
      <div class="curator_line"></div><span>20</span>
      </div>`);
 
+     $("#player").hide('slow')
+     var homeToMain = new TimelineMax({});
+	
+     // Hide
+     $('.logo-text').css('display', 'none'),
+     homeToMain.to($('.line, .text-wrap'), 0.5, {display: 'none', opacity: 0, y: -20, ease: Power2.easeInOut}, 0),
+     
+     // Background down
+     homeToMain.to($('.wave-container'), 1, {yPercent: 30, ease: Power2.easeInOut}, 0),
+         
+     // Show
+     $('#curator').css('display', 'block'),
+     homeToMain.fromTo($('.back_btn'), 0.8, {x: 15},
+                                         {display: 'flex', opacity: 1, x: 0, ease: Power2.easeInOut}, 1),
+         
+     homeToMain.fromTo($('.curator_title_wrapper'), 0.8, {opacity: 0, x: 30},
+                                         {opacity: 1, x: 0, ease: Power2.easeInOut}, 1),
+         
+     homeToMain.fromTo($('.curator_list'), 0.8, {opacity: 0, display: 'none', x: 30},
+                                     {opacity: 1, x: 0, display: 'block', ease: Power2.easeInOut}, 1.2)
+     
     getLyricsAndDisplay(musixMatchQuery);
 });
